c15a859488567152610303352ed992f49870b92d
FraVirgu <francesco.virgulti@mail.polimi.it>
2025-12-16 16:30:29 +0100

    Everithink is working

--- applications/modules/shockThermo/shockThermo.C
+++ applications/modules/shockThermo/shockThermo.C
@@ -1,136 +1,125 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2023 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
 #include "shockThermo.H"
+#include "fvMeshStitcher.H"
+#include "localEulerDdtScheme.H"
+#include "hydrostaticInitialisation.H"
+#include "fvcMeshPhi.H"
+#include "fvcVolumeIntegrate.H"
+#include "fvcReconstruct.H"
+#include "fvcSnGrad.H"
 #include "addToRunTimeSelectionTable.H"
-#include "fvModels.H"
-#include "fvConstraints.H"
+
+#include "psiThermo.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
 
 namespace Foam
 {
     namespace solvers
     {
-        // 1. Register the solver
         defineTypeNameAndDebug(shockThermo, 0);
         addToRunTimeSelectionTable(solver, shockThermo, fvMesh);
+    }
+}
 
-        // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
 
-        shockThermo::shockThermo(fvMesh &mesh)
-            : // Initialize Base Class (Creates the thermo from dictionary)
-              shockFluidThermo(mesh, autoPtr<fluidThermo>(nullptr)),
+// * * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * //
 
-              // Initialize References
-              thermo_(refCast<fluidMulticomponentThermo>(thermoPtr_())),
-              Y_(thermo_.Y()),
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
-              // Initialize Models
-              reaction(combustionModel::New(thermo_, momentumTransport())),
+Foam::solvers::shockThermo::shockThermo(fvMesh &mesh)
+    :
 
-              thermophysicalTransport(
-                  fluidMulticomponentThermophysicalTransportModel::New(
-                      momentumTransport(),
-                      thermo_)),
+      shockFluid(
+          mesh,
+          autoPtr<fluidThermo>(fluidMulticomponentThermo::New(mesh).ptr())),
 
-              // --- Initialize Wrapper ---
-              // We pass the reference 'thermo_' to your wrapper
-              heThermo_(new highEnthalpyMulticomponentThermo(thermo_)),
+      thermo_(refCast<fluidMulticomponentThermo>(shockFluid::thermo_)),
 
-              // Initialize Public References
-              thermo(thermo_),
-              Y(Y_)
+      Y_(thermo_.Y()),
+
+      reaction(combustionModel::New(thermo_, momentumTransport())),
+
+      thermophysicalTransport(
+          fluidMulticomponentThermophysicalTransportModel::New(
+              momentumTransport(),
+              thermo_)),
+      thermo(thermo_),
+      Y(Y_)
+{
+    thermo.validate(type(), "h", "e");
+
+    forAll(Y, i)
+    {
+        fields.add(Y[i]);
+    }
+    fields.add(thermo.he());
+}
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::solvers::shockThermo::~shockThermo()
+{
+}
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+void Foam::solvers::shockThermo::preSolve()
+{
+    // cast the thermo class to support multicomponent fluid.
+    refCast<fluidMulticomponentThermo>(shockFluid::thermo_);
+
+    {
+        const surfaceScalarField amaxSf(
+            max(mag(aphiv_pos()), mag(aphiv_neg())));
+
+        if (transient())
         {
-            // Validation
-            thermo.validate(type(), "h", "e");
-
-            // Collect fields for interpolation
-            forAll(Y_, i)
-            {
-                fields.add(Y_[i]);
-            }
-            fields.add(thermo.he());
+            correctCoNum(amaxSf);
         }
-
-        // Destructor
-        shockThermo::~shockThermo() = default;
-
-        // * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
-
-        void shockThermo::preSolve()
+        else if (LTS)
         {
-            shockFluidThermo::preSolve();
+            setRDeltaT(amaxSf);
         }
+    }
 
-        void shockThermo::thermophysicalPredictor()
-        {
-            // 1. Convection scheme
-            tmp<fv::convectionScheme<scalar>> mvConvection(
-                fv::convectionScheme<scalar>::New(
-                    mesh,
-                    fields,
-                    phi,
-                    mesh.schemes().div("div(phi,Yi_h)")));
+    fvModels().preUpdateMesh();
 
-            // 2. Reaction
-            reaction->correct();
+    if (mesh.topoChanging() || mesh.stitcher().stitches())
+    {
+        pos.clear();
+        neg.clear();
 
-            // 3. Species Transport
-            forAll(Y_, i)
-            {
-                volScalarField &Yi = Y_[i];
-                if (thermo_.solveSpecie(i))
-                {
-                    fvScalarMatrix YiEqn(
-                        fvm::ddt(rho, Yi) + mvConvection->fvmDiv(phi, Yi) + thermophysicalTransport->divj(Yi) ==
-                        reaction->R(Yi) + fvModels().source(rho, Yi));
+        clearTemporaryFields();
+    }
 
-                    YiEqn.relax();
-                    fvConstraints().constrain(YiEqn);
-                    YiEqn.solve("Yi");
-                    fvConstraints().constrain(Yi);
-                }
-            }
+    // Update the mesh for topology change, mesh to mesh mapping
+    mesh_.update();
+}
 
-            // 4. Normalise
-            thermo_.normaliseY();
-
-            // 5. Energy Equation setup
-            volScalarField &e = thermo_.he();
-
-            // Interpolate energy
-            const surfaceScalarField e_pos(interpolate(e, pos, thermo_.T().name()));
-            const surfaceScalarField e_neg(interpolate(e, neg, thermo_.T().name()));
-
-            // Convective flux
-            surfaceScalarField phiEp(
-                "phiEp",
-                aphiv_pos() * (rho_pos() * (e_pos + 0.5 * magSqr(U_pos())) + p_pos()) + aphiv_neg() * (rho_neg() * (e_neg + 0.5 * magSqr(U_neg())) + p_neg()) + aSf() * (p_pos() - p_neg()));
-
-            if (mesh.moving())
-            {
-                phiEp += mesh.phi() * (a_pos() * p_pos() + a_neg() * p_neg());
-            }
-
-            fvScalarMatrix EEqn(
-                fvm::ddt(rho, e) + fvc::div(phiEp) + fvm::ddt(rho, K) ==
-                fvModels().source(rho, e));
-
-            if (!inviscid)
-            {
-                const surfaceScalarField devTauDotU(
-                    "devTauDotU",
-                    devTau() & (a_pos() * U_pos() + a_neg() * U_neg()));
-                EEqn += thermophysicalTransport->divq(e) + fvc::div(devTauDotU);
-            }
-
-            // 6. Solve Energy
-            EEqn.relax();
-            fvConstraints().constrain(EEqn);
-            EEqn.solve();
-            fvConstraints().constrain(e);
-
-            // 7. Update Thermodynamics
-            // --- HERE IS THE MAGIC ---
-            // Instead of thermo_.correct(), we call the wrapper!
-            heThermo_->correct();
-        }
-
-    } // namespace solvers
-} // namespace Foam
+// ************************************************************************* //
--- src/thermophysicalModels/multicomponentThermo/highEnthalpyMulticomponentThermo/highEnthalpyMulticomponentThermo.C
+++ src/thermophysicalModels/multicomponentThermo/highEnthalpyMulticomponentThermo/highEnthalpyMulticomponentThermo.C
@@ -1,111 +1,54 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2023 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
 #include "highEnthalpyMulticomponentThermo.H"
-#include "dimensionedScalar.H"
-#include "IOdictionary.H"
-#include "volFields.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
 
 namespace Foam
 {
     defineTypeNameAndDebug(highEnthalpyMulticomponentThermo, 0);
-
-    // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
-
-    highEnthalpyMulticomponentThermo::highEnthalpyMulticomponentThermo(
-        fluidMulticomponentThermo &wrapped,
-        mutationMixture &mix)
-        : wrappedThermo_(wrapped),
-          mutationMix_(mix),
-          Tve_(
-              IOobject(
-                  "Tve",
-                  wrapped.T().time().name(),
-                  wrapped.T().mesh(),
-                  IOobject::MUST_READ,
-                  IOobject::AUTO_WRITE),
-              wrapped.T().mesh())
-    {
-        Info << "highEnthalpyMulticomponentThermo: Linked to "
-             << wrappedThermo_.thermoName()
-             << " and Mutation++ mechanism." << endl;
-    }
-
-    // * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
-
-    // Forwarding
-    const volScalarField &highEnthalpyMulticomponentThermo::T() const { return wrappedThermo_.T(); }
-    volScalarField &highEnthalpyMulticomponentThermo::T() { return wrappedThermo_.T(); }
-
-    const volScalarField &highEnthalpyMulticomponentThermo::p() const { return wrappedThermo_.p(); }
-    volScalarField &highEnthalpyMulticomponentThermo::p() { return wrappedThermo_.p(); }
-
-    const PtrList<volScalarField> &highEnthalpyMulticomponentThermo::Y() const { return wrappedThermo_.Y(); }
-    PtrList<volScalarField> &highEnthalpyMulticomponentThermo::Y() { return wrappedThermo_.Y(); }
-
-    const volScalarField &highEnthalpyMulticomponentThermo::Cp() const { return wrappedThermo_.Cp(); }
-    const volScalarField &highEnthalpyMulticomponentThermo::Cv() const { return wrappedThermo_.Cv(); }
-
-    const volScalarField &highEnthalpyMulticomponentThermo::he() const { return wrappedThermo_.he(); }
-    volScalarField &highEnthalpyMulticomponentThermo::he() { return wrappedThermo_.he(); }
-
-    scalar highEnthalpyMulticomponentThermo::readEnthalpyCorrection() const
-    {
-        return 0.0;
-    }
-
-    // ------------------------------------------------------------------------
-    // THE 2-TEMPERATURE UPDATE LOOP
-    // ------------------------------------------------------------------------
-    void highEnthalpyMulticomponentThermo::correct()
-    {
-        // 1. Run standard update (Calculates T from current energy, updates transport)
-        wrappedThermo_.correct();
-
-        // 2. OVERWRITE Energy with Non-Equilibrium Physics (T_tr != T_ve)
-        Info << "HighEnthalpy: Updating 2-T Physics via Mutation++..." << endl;
-
-        const volScalarField &p = wrappedThermo_.p();
-        const volScalarField &Ttr = wrappedThermo_.T();
-        const PtrList<volScalarField> &Y = wrappedThermo_.Y();
-
-        // Access the Energy Field (This is 'e' internal energy in shockThermo)
-        volScalarField &e_field = wrappedThermo_.he();
-
-        // NOTE: We do NOT update Cp, mu, alpha here.
-        // In the Wrapper pattern, these are read-only from the base thermo.
-        // We rely on the base models (Janaf/Sutherland) for transport,
-        // but we enforce strict Energy Conservation using Mutation++.
-
-        forAll(Ttr, celli)
-        {
-            // Gather Data
-            scalar P_c = p[celli];
-            scalar T_c = Ttr[celli];
-            scalar Tve_c = Tve_[celli];
-
-            // Build local mass fractions
-            scalarField Y_c(Y.size());
-            forAll(Y, i) Y_c[i] = Y[i][celli];
-
-            // CALL MUTATION++
-            // Sets the state using T_tr AND T_ve
-            mutationMix_.setState(P_c, T_c, Tve_c, Y_c);
-
-            // GET ENTHALPY [J/kg]
-            double H_mut = mutationMix_.H();
-
-            // CONVERT TO INTERNAL ENERGY [J/kg]
-            // e = h - p/rho = h - R_mixture * T
-            // R_mixture = Ru / M_mixture
-
-            double Mw_mix = mutationMix_.mixtureMw(); // Calls your new helper
-            double Ru = mutationMix_.Ru();            // Calls your new helper
-            double R_mix = Ru / Mw_mix;               // J/(kg K)
-
-            // Update the OpenFOAM field
-            e_field[celli] = H_mut - (R_mix * T_c);
-        }
-
-        // 3. Correct Boundaries
-        e_field.correctBoundaryConditions();
-        Tve_.correctBoundaryConditions();
-    }
+    defineRunTimeSelectionTable(highEnthalpyMulticomponentThermo, fvMesh);
 }
+
+// * * * * * * * * * * * * * * * * Selectors * * * * * * * * * * * * * * * * //
+
+Foam::autoPtr<Foam::highEnthalpyMulticomponentThermo> Foam::highEnthalpyMulticomponentThermo::New
+(
+    const fvMesh& mesh,
+    const word& phaseName
+)
+{
+    return basicThermo::New<highEnthalpyMulticomponentThermo>(mesh, phaseName);
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::highEnthalpyMulticomponentThermo::~highEnthalpyMulticomponentThermo()
+{}
+
+
+// ************************************************************************* //
